2fabbd61662a99cc335221b6157057935a598403
Yejin Jung <yejiin.jung@gmail.com>
2019-02-11 14:02:22 +0800

    Seeded with API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+data = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+array = data["drinks"]
+array.each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
